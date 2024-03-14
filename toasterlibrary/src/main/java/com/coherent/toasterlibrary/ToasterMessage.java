7c6a8e81e77297d213b67028a5db41b4e863dbca
sivaprasath <sivaprasath.selvam@coherent.in>
2024-03-14 14:10:05 +0530

    version1.10
    version1.10

--- toasterlibrary/src/main/java/com/coherent/toasterlibrary/ToasterMessage.java
+++ toasterlibrary/src/main/java/com/coherent/toasterlibrary/ToasterMessage.java
@@ -4,7 +4,13 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class ToasterMessage {
-    public static void s(Context c, String message){
+    public static void shortToast(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+
+    public static void LongToast(Context c, String message){
 
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
 
